efbb2977a0e707b73cd219fa6c5b70bdab6e2466
mohamedvall <mohamed.val0520@gmail.com>
2025-11-14 23:16:20 +0000

    Initial Spring Boot project for server management TD

---
+++ src/main/java/com/td/server_management_td/ServerManagementTdApplication.java
@@ -0,0 +1,13 @@
+package com.td.server_management_td;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagementTdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagementTdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/td/server_management_td/ServerManagementTdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.server_management_td;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementTdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
